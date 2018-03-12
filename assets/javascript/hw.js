f400586c5c0dd4bfcb1e8449c9c92b04c8427874
Your Name <you@example.com>
2018-03-11 21:43:50 -0400

    add a button bro

--- assets/javascript/hw.js
+++ assets/javascript/hw.js
@@ -1,1 +1,33 @@
 var animals = ["cats", "dogs", "parrots", "birds", "lioncubs"];
+
+//Function for adding button for the searched animal
+function renderButtons(){
+
+    // To empty the buttons already existing while loading to prevent repetition
+    $("#buttonDiv").empty();
+
+    
+
+    //create a new button every time for the animals in the array in every iteration
+    for (var i=0; i<animals.length; i++){
+        var a = $("<button>");
+        a.addClass("animal");
+        a.attr("data-name", animals[i]);
+        a.text(animals[i]);
+        $("#buttonDiv").append(a);
+    }
+        
+}
+
+//On clicking submit, a button with the search item will be added to the button array
+$("#add-animal").on("click", function(event){
+    event.preventDefault();
+
+    var animal = $("animal-input").val();
+    console.log(animal);
+    animals.push(animal);
+    console.log(animals);
+    renderButtons();
+});
+renderButtons();
+
