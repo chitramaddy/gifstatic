75b07d239e9d5a812fd55ed3e39941ede1a86295
Your Name <you@example.com>
2018-03-12 14:26:53 -0400

    button clicked adds animal stills to the page

--- assets/javascript/hw.js
+++ assets/javascript/hw.js
@@ -1,33 +1,78 @@
-var animals = ["cats", "dogs", "parrots", "birds", "lioncubs"];
+var animals = ["cats", "dogs", "parrots", "birds", "lions"];
+
+function displayAnimals() {
+    var animal = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=NNTsl0EV1lnlP0srz9RMTwD2f6g8cjoc&q=" + animal + "&limit=10";
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+
+        var results = response.data;
+
+        for (var i=0; i<results.length; i++){
+            var gifDiv = $("<div class='item'>");
+            var rating = results[i].rating;
+
+            var p = $("<p>").text("Rating: "+ rating)
+            var animalImage = $("<img>");
+
+            animalImage.attr("src", results[i].images.original_still.url);
+
+            gifDiv.append(p);
+            gifDiv.append(animalImage);
+
+            $("#animals-view").prepend(gifDiv);
+
+        }
+
+            renderButtons();
+    })
+}
 
 //Function for adding button for the searched animal
-function renderButtons(){
+function renderButtons() {
 
     // To empty the buttons already existing while loading to prevent repetition
     $("#buttonDiv").empty();
 
-    
+
 
     //create a new button every time for the animals in the array in every iteration
-    for (var i=0; i<animals.length; i++){
+    for (var i = 0; i < animals.length; i++) {
+
         var a = $("<button>");
-        a.addClass("animal");
+        a.addClass("animal-btn");
         a.attr("data-name", animals[i]);
         a.text(animals[i]);
         $("#buttonDiv").append(a);
     }
-        
+
 }
 
 //On clicking submit, a button with the search item will be added to the button array
-$("#add-animal").on("click", function(event){
+$("#add-animal").on("click", function (event) {
+
+    //This will prevent the page reloading after every time the submit button is clicked.
     event.preventDefault();
 
-    var animal = $("animal-input").val();
-    console.log(animal);
-    animals.push(animal);
-    console.log(animals);
-    renderButtons();
-});
-renderButtons();
+    if ($("#animal-input").val() !== " ") {
 
+        var animal = $("#animal-input").val();
+        console.log(animal);
+
+
+        $("#animal-input").val(" ");
+        animals.push(animal);
+        console.log(animals);
+        renderButtons();
+    }
+});
+
+
+
+$(document).on("click", ".animal-btn", displayAnimals);
+
+renderButtons();
